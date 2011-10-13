a9b4da7c1d7096b38c571a4e545cbb659b90bbc0
Marcos Almonacid <amilcar.54@gmail.com>
2011-10-13 13:16:34 -0300

    Added strings commands tests: getset,mget,mset,msetnx,setex,setnx,strlen

--- test/strings_SUITE.erl
+++ test/strings_SUITE.erl
@@ -5,10 +5,8 @@
 
 all() ->
 	[set_and_get,append,decr,incr,decrby,incrby,
-	 setbit,getbit,setrange,getrange].
-%%   getset,mget
-%% 	 mset,msetnx,,setex,
-%% 	 setnx,,strlen].
+	 setbit,getbit,setrange,getrange,mset,mget,
+	 setnx,msetnx,setex,getset,strlen].
 
 init_per_testcase(_TestCase,Config) ->
 	{ok,Client} = connect_erldis(10),
@@ -231,3 +229,100 @@ getrange(Config) ->
 	<<"orld">> = erldis_client:sr_scall(Client,[<<"getrange">>,<<"mykey">>,<<"-4">>,<<"-1">>]),
 	<<" World">> = erldis_client:sr_scall(Client,[<<"getrange">>,<<"mykey">>,<<"5">>,<<"100">>]),
 	<<"Hello World">> = erldis_client:sr_scall(Client,[<<"getrange">>,<<"mykey">>,<<"-500">>,<<"100">>]).
+
+mset(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),
+	
+	ok = erldis_client:sr_scall(Client,[<<"mset">>,<<"x">>,<<"10">>,<<"y">>,<<"foo bar">>,<<"z">>,<<"x x x x x x x\n\n\r\n">>]),
+	<<"10">> = erldis_client:sr_scall(Client,[<<"get">>,<<"x">>]),
+	<<"foo bar">> = erldis_client:sr_scall(Client,[<<"get">>,<<"y">>]),
+	<<"x x x x x x x\n\n\r\n">> = erldis_client:sr_scall(Client,[<<"get">>,<<"z">>]),
+	
+	{error,<<"ERR wrong number of arguments for 'MSET' command">>} = erldis_client:sr_scall(Client,[<<"mset">>,<<"x">>,<<"10">>,<<"y">>,<<"foo bar">>,<<"z">>]).
+	
+mget(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),
+	
+	ok = erldis_client:sr_scall(Client,[<<"set">>,<<"foo">>,<<"FOO">>]),
+	ok = erldis_client:sr_scall(Client,[<<"set">>,<<"bar">>,<<"BAR">>]),
+	[<<"BAR">>,<<"FOO">>] = erldis_client:scall(Client,[<<"mget">>,<<"bar">>,<<"foo">>]),
+	[<<"BAR">>,nil,<<"FOO">>] = erldis_client:scall(Client,[<<"mget">>,<<"bar">>,<<"bazz">>,<<"foo">>]),
+	[<<"FOO">>,nil,<<"BAR">>,nil] = erldis_client:scall(Client,[<<"mget">>,<<"foo">>,<<"bazz">>,<<"bar">>,<<"myset">>]),
+	
+	{error,<<"ERR wrong number of arguments for 'MGET' command">>} = erldis_client:sr_scall(Client,[<<"mget">>]).
+
+setnx(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),
+	
+	%% with not existing key
+	true = erldis_client:sr_scall(Client,[<<"setnx">>,<<"x">>,<<"10">>]),
+	<<"10">> = erldis_client:sr_scall(Client,[<<"get">>,<<"x">>]),
+	
+	%% with already existent key
+	false = erldis_client:sr_scall(Client,[<<"setnx">>,<<"x">>,<<"35">>]),
+	<<"10">> = erldis_client:sr_scall(Client,[<<"get">>,<<"x">>]),
+	
+	%% SETNX will overwrite EXPIREing key
+	true = erldis_client:sr_scall(Client,[<<"expire">>,<<"x">>,<<"100">>]),
+    true = erldis_client:sr_scall(Client,[<<"setnx">>,<<"x">>,<<"888">>]),
+	<<"888">> = erldis_client:sr_scall(Client,[<<"get">>,<<"x">>]),
+	
+	{error,<<"ERR wrong number of arguments for 'SETNX' command">>} = erldis_client:sr_scall(Client,[<<"setnx">>,<<"x">>]).
+	
+msetnx(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),    
+	
+	%% with not existing keys
+	true = erldis_client:sr_scall(Client,[<<"msetnx">>,<<"x">>,<<"10">>,<<"y">>,<<"foo bar">>]),
+	<<"10">> = erldis_client:sr_scall(Client,[<<"get">>,<<"x">>]),
+	<<"foo bar">> = erldis_client:sr_scall(Client,[<<"get">>,<<"y">>]),
+	
+	%% with already existent key
+	false = erldis_client:sr_scall(Client,[<<"msetnx">>,<<"x">>,<<"22">>,<<"w">>,<<"foo">>,<<"z">>,<<"bar">>]),
+	<<"10">> = erldis_client:sr_scall(Client,[<<"get">>,<<"x">>]),
+	nil = erldis_client:sr_scall(Client,[<<"get">>,<<"w">>]),
+	nil = erldis_client:sr_scall(Client,[<<"get">>,<<"z">>]),
+	
+	{error,<<"ERR wrong number of arguments for 'MSETNX' command">>} = erldis_client:sr_scall(Client,[<<"msetnx">>,<<"x">>,<<"22">>,<<"w">>]),
+	{error,<<"ERR wrong number of arguments for 'MSETNX' command">>} = erldis_client:sr_scall(Client,[<<"msetnx">>]).
+	
+setex(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),
+	
+	ok = erldis_client:sr_scall(Client,[<<"setex">>,<<"mykey">>,<<"6">>,<<"FOO">>]),
+	<<"FOO">> = erldis_client:sr_scall(Client,[<<"get">>,<<"mykey">>]),
+	timer:sleep(3000),
+	<<"FOO">> = erldis_client:sr_scall(Client,[<<"get">>,<<"mykey">>]),
+	timer:sleep(4500),
+	nil = erldis_client:sr_scall(Client,[<<"get">>,<<"mykey">>]),
+	
+	{error,<<"ERR invalid expire time in SETEX">>} = erldis_client:sr_scall(Client,[<<"setex">>,<<"z">>,<<"-30">>,<<"bar">>]),
+	{error,<<"ERR wrong number of arguments for 'SETEX' command">>} = erldis_client:sr_scall(Client,[<<"setex">>,<<"z">>,<<"5">>]).
+
+getset(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),
+	
+	%% Set new value
+    nil = erldis_client:sr_scall(Client,[<<"getset">>,<<"foo">>,<<"xyz">>]),
+	<<"xyz">> = erldis_client:sr_scall(Client,[<<"get">>,<<"foo">>]),
+
+	%% Replace old value
+	ok = erldis_client:sr_scall(Client,[<<"set">>,<<"foo">>,<<"bar">>]),
+    <<"bar">> = erldis_client:sr_scall(Client,[<<"getset">>,<<"foo">>,<<"buzz">>]),    
+	<<"buzz">> = erldis_client:sr_scall(Client,[<<"get">>,<<"foo">>]),
+    
+	{error,<<"ERR wrong number of arguments for 'GETSET' command">>} = erldis_client:sr_scall(Client,[<<"getset">>,<<"foo">>]),
+	{error,<<"ERR wrong number of arguments for 'GETSET' command">>} = erldis_client:sr_scall(Client,[<<"getset">>,<<"foo">>,<<"bar">>,<<"buzz">>]).
+
+strlen(Config) ->
+	{client,Client} = lists:keyfind(client, 1, Config),
+	
+	%% With already existent key
+	ok = erldis_client:sr_scall(Client,[<<"set">>,<<"mykey">>,<<"Hello World">>]),
+	11 = erldis_client:sr_scall(Client,[<<"strlen">>,<<"mykey">>]),
+	
+	%% With not existing key
+	false = erldis_client:sr_scall(Client,[<<"strlen">>,<<"nonexisting">>]),
+	
+	{error,<<"ERR wrong number of arguments for 'STRLEN' command">>} = erldis_client:sr_scall(Client,[<<"strlen">>,<<"foo">>,<<"bar">>]),
+	{error,<<"ERR wrong number of arguments for 'STRLEN' command">>} = erldis_client:sr_scall(Client,[<<"strlen">>]).
